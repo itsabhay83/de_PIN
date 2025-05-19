dbeab1532d810050416d65cdf08753d19685cc75
itsabhay83 <22bec003@nith.ac.in>
2025-05-19 11:20:14 +0530

    added the schema

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -1,11 +1,9 @@
-import type { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 export function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const authHeader = req.headers["authorization"];
-  req.userId = "1";
   next();
 }
